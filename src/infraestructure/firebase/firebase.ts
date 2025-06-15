8b111903abe906695aad228aec12b1c73cc71045
Brayan <Thealexander@users.noreply.github.com>
2025-06-15 11:19:43 -0700

    actualizacion de variables de entorno

--- src/infraestructure/firebase/firebase.ts
+++ src/infraestructure/firebase/firebase.ts
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
 import admin from "firebase-admin";
-import * as serviceAccount from "../../../config/serviceAccountkey.json";
+
+const serviceAccountPath =
+  process.env.SERVICE_ACCOUNT_PATH ||
+  path.resolve(__dirname, "../../../config/serviceAccountKey.json");
+
+if (!fs.existsSync(serviceAccountPath)) {
+  throw new Error(
+    `[Firebase Init] serviceAccountKey file not found at: ${serviceAccountPath}`
+  );
+}
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 if (!admin.apps.length) {
   admin.initializeApp({
